8a315148351a2c7fdada35a19ecd6224139935c1
Alex Mengran Wu <awu3@dsg1.virtual.crc.nd.edu>
2014-11-02 18:27:06 -0500

    preliminary html setup

--- index.php
+++ index.php
@@ -1,13 +1,27 @@
 <html>
 	<head>
 <meta charset="utf-8">
-		<meta name="author" content="Alex Wu , Ethan Chen, Norman Morales">
+		<meta name="author" content="Alex Wu , Ethan Chen">
 		<meta name="description" content="Prepare for your next technical interview with EmployMe.">
 		<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1" />
 
 	</head>
 
 	<body>
-	<?php echo "Hello World"; ?>
+<!-- 		<?php echo "Hello World"; ?>-->
+		<div class="section" id="banner">
+			<img src="/img/tux.svg" alt="Tux">
+		</div>
+
+		<!-- later, will pull popular questions based on the database-->
+		<div class="section" id="featured">
+
+
+		</div>
+		
+		<div class="section" id="about">
+
+	
+		</div>
 	</body>
 </html>
