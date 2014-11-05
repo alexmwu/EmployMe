07cdd32a20125e42e54baa3b841ac08abba28e0f
Alex Wu <awu3@nd.edu>
2014-11-05 03:32:22 -0500

    added question posting ability

--- index.php
+++ index.php
@@ -8,21 +8,27 @@
 	</head>
 
 	<body>
-<!-- 		<?php echo "Hello World"; ?>-->
-<!--		<a href="sign_up.php">Sign Up</a>-->
+		<!--header here-->
+
+
+		<!-- banner for the site-->
 		<div class="section" id="banner">
 			<a href="sign_up.php"><img src="img/tux.svg" alt="Tux"></a>
+			<a href="post_question.php"><img src="img/mysql.svg" alt="MySQL"></a>
 		</div>
 
-		<!-- later, will pull popular questions based on the database-->
+		<!-- some of the best example questions-->
 		<div class="section" id="featured">
 
 
 		</div>
-		
+	
+		<!--a blurb on EmployMe-->	
 		<div class="section" id="about">
 
 	
 		</div>
+
+		<!--foother here-->
 	</body>
 </html>
