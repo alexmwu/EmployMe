757576341cc7085ac84f84aa321a026f7fa7b8ac
Alex Wu <mwumbojumbo@gmail.com>
2014-11-04 23:30:14 -0500

    fixed php parsing errors

--- create_user.php
+++ create_user.php
@@ -1,26 +1,18 @@
-hi
-
-<html><head></head><body>
-
 <?php
-echo "<p>hiiiii</p>";
+
 //connecting
 
-$link=mysqli_connect('localhost','awu3','hellomysql');
-if($link->connect_errno){
-	echo "Problem ".$link->connect_errno.": ".$link->connect_error;
-	die();
-}
+$link=mysqli_connect('localhost','awu3','hellomysql') or die("Problem connecting to server.");
 
 //use database awu3
 mysql_select_db('awu3') or die('Could not use database!');
 
 //prepare statement
 $today=date("Y/m/d");
-$statement=$link->prepare("insert into users(username) values(?);"//,first_name,last_name,school,major,graduation_time,created) values(?,?,?,?,?,?);");
+$statement=$link->prepare("insert into users values(username,first_name,last_name,school,major,graduation_time,created) values(?,?,?,?,?,?);");
 
 //bind
-$statement->bind_param("s"/*ssssiss"*/,$_POST['username');//,'first_name','last_name','school','major','graduation'],$today);
+$statement->bind_param("sssssiss",$_POST['username'],$_POST['first_name'],$_POST['last_name'],$_POST['school'],$_POST['major'],$_POST['graduation'],$today);
 
 //execute
 $statement->execute();
@@ -28,7 +20,3 @@ $statement->execute();
 //close
 mysqli_close($link);
 ?>
-
-
-</body>
-</html>
