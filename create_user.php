c56f8f0c0e51951601b6189dd78632fe3aadd9fa
Alex Wu <awu3@nd.edu>
2014-11-05 01:47:19 -0500

    creating new user now works

--- create_user.php
+++ create_user.php
@@ -2,20 +2,21 @@
 
 //connecting
 
-$link=mysqli_connect('localhost','awu3','hellomysql') or die("Problem connecting to server.");
+$link=mysqli_connect('localhost','awu3','hellomysql','awu3') or die("Problem connecting to server.");
 
 //use database awu3
-mysql_select_db('awu3') or die('Could not use database!');
+//mysql_select_db('awu3') or die('Could not use database!');
 
 //prepare statement
 $today=date("Y/m/d");
-$statement=$link->prepare("insert into users values(username,first_name,last_name,school,major,graduation_time,created) values(?,?,?,?,?,?);");
+$statement=$link->prepare("INSERT INTO users (username,first_name,last_name,school,major,score,graduation_time,created) values(?,?,?,?,?,?,?,?);");
 
 //bind
-$statement->bind_param("sssssiss",$_POST['username'],$_POST['first_name'],$_POST['last_name'],$_POST['school'],$_POST['major'],$_POST['graduation'],$today);
+$init=0;
+$statement->bind_param("sssssiss",$_POST['username'],$_POST['first_name'],$_POST['last_name'],$_POST['school'],$_POST['major'],$init,$_POST['graduation'],$today);
 
 //execute
-$statement->execute();
+$statement->execute() or die("Username already exists");
 
 //close
 mysqli_close($link);
