fbe9cc86f226ee2de41668402d1b767a4ea50220
Ethan Yuxuan Chen <ychen18@nd.edu>
2014-12-17 08:53:10 -0500

    add d3 visualization

--- questions/questions.php
+++ questions/questions.php
@@ -75,10 +75,7 @@ $link = mysqli_connect('localhost', 'awu3', 'hellomysql', 'awu3') or die("Proble
     <![endif]-->
 
 <div class="main grey">
-                <div class="main wrapper clearfix">
-                    <article>
-                        <h1>"This site rocks!" - Mark Zuckerberg</h1>
-                    </article>
+                <div class="main wrapper clearfix" id="d3-cloud">
                 </div>
             </div> <!-- #main -->
     
@@ -209,5 +206,43 @@ mysqli_close($link);
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
 
         <script src="../js/main.js"></script>
+
+        <script src="../js/d3.js"></script>
+<script src="../js/d3.layout.cloud.js"></script>
+<script>
+  var fill = d3.scale.category20();
+
+  d3.layout.cloud().size([300, 300])
+      .words([
+        "Hello", "world", "normally", "you", "want", "more", "words",
+        "than", "this"].map(function(d) {
+        return {text: d, size: 10 + Math.random() * 90};
+      }))
+      .padding(5)
+      .rotate(function() { return ~~(Math.random() * 2) * 90; })
+      .font("Impact")
+      .fontSize(function(d) { return d.size; })
+      .on("end", draw)
+      .start();
+
+  function draw(words) {
+    d3.select("#d3-cloud").append("svg")
+        .attr("width", 300)
+        .attr("height", 300)
+      .append("g")
+        .attr("transform", "translate(150,150)")
+      .selectAll("text")
+        .data(words)
+      .enter().append("text")
+        .style("font-size", function(d) { return d.size + "px"; })
+        .style("font-family", "Impact")
+        .style("fill", function(d, i) { return fill(i); })
+        .attr("text-anchor", "middle")
+        .attr("transform", function(d) {
+          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function(d) { return d.text; });
+  }
+</script>
         </body>
 </html>
